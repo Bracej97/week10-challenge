d4277d0f9df2178684348113c5f2e4fe47e9060a
Joe Bracey <joebracey1@gmail.com>
2024-11-24 16:50:37 +0000

    Cart summary fucntion created

---
+++ src/components/CartSummary.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import { BasketContext } from '../contexts/BasketContext'
+
+const CartSummary = () => {
+    const { basket, calculateTotal } = useContext(BasketContext);
+
+    return (
+        <div>{basket.length} item(s) in your basket: £{calculateTotal()}</div>
+    )
+}
