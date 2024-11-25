a0ae1d8e84c859729907d174a5e13628e6c98a7d
Joe Bracey <joebracey1@gmail.com>
2024-11-25 13:35:01 +0000

    Images added to the product cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import { BasketContext } from "../contexts/BasketContext";
 import { useContext } from "react";
 import { Link } from 'react-router-dom';
 import { products } from '../data';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Button, CardActions, CardMedia, Typography } from "@mui/material";
+
 
 function Home() {
     const { addToBasket } = useContext(BasketContext);
@@ -10,15 +14,34 @@ function Home() {
     return (
         <div>
             <h1>Products</h1>
-            <div>
+            <div style={{
+                width: '100%',
+                display: 'flex',
+                flex: 1,
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContent: 'space-evenly',
+                alignSelf: 'stretch',
+                gap: '10px',
+             }}>
                 {products.map((product) => (
-                    <div key={product.id} className="product-card">
-                        <h2>{product.name}</h2>
-                        <p>Price: £{product.price}</p>
-                        <Link to={`/product/${product.id}`}>View product</Link>
-                        <button onClick={() => addToBasket(product)}>Add to basket</button>
-                    </div>
-                ))};
+                    <Card sx={{ maxWidth: 345 }} key={product.id}>
+                        <CardContent>
+                            <CardMedia sx={{ height: 230 }} image={`../../public/${product.id}.jpg`}
+                            />
+                            <Typography gutterBottom variant="h5" component="div">
+                                {product.name}
+                            </Typography>
+                            <Typography variant="body2" sx={{ color: 'text-secondary' }}>
+                                Price: £{product.price}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small"><Link to={`/product/${product.id}`}>View product</Link></Button>
+                            <Button size="small" onClick={() => addToBasket(product)}>Add to basket</Button>
+                        </CardActions>
+                    </Card>
+                ))}
             </div>
         </div>
     )
