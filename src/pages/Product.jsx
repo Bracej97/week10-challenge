d0bed90623dea1bf3af3756c7d424712d9f61d36
Joe Bracey <joebracey1@gmail.com>
2024-11-27 21:25:13 +0000

    card format added to product detail with description

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,11 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { BasketContext } from "../contexts/BasketContext";
 import { products } from "../data";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActions, CardMedia, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 function Product() {
     const { addToBasket } = useContext(BasketContext);
@@ -11,10 +16,29 @@ function Product() {
     if (!product) return <p>Product not found.</p>
 
     return (
-        <div className="product-card">
-            <h2>{product.name}</h2>
-            <p>Price: £{product.price}</p>
-            <button onClick={() => {addToBasket(product)}}>Add to basket</button>
+        <div className="product-card" >
+            <div style={{ height: '60px' }} />
+            <Card sx={{ maxWidth: 500, minWidth: 300, backgroundColor: '#444444'}} key={product.id}>
+                        <CardContent>
+                            <CardMedia sx={{ height: 300, marginBottom: "5px" }} image={`/${product.id}.jpg`}
+                            />
+                            <Typography gutterBottom variant="h5" component="div" sx={{ color: '#ffffff' }}>
+                                {product.name}
+                            </Typography>
+                            <Typography variant="body1" sx={{ color: '#ffffff' }} gutterBottom>
+                                {product.description}
+                            </Typography>
+
+                        </CardContent>
+                        <CardActions style={{justifyContent: 'center'}}>
+                            <IconButton sx={{ color:"#4dabf5" }} aria-label="add to shopping cart" onClick={() => addToBasket(product)}>
+                                <Typography variant="h5" sx={{ color: '#ffffff', whiteSpace: 'pre-line', marginRight: '10px' }}>
+                                    £{product.price}
+                                </Typography>
+                                <AddShoppingCartIcon />
+                            </IconButton>
+                        </CardActions>
+                    </Card>
         </div>
     );
 }
