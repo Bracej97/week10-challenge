9e6b60ccd28414e3495c0af6333a1321b0009253
Joe Bracey <joebracey1@gmail.com>
2024-11-28 08:50:03 +0000

    Changed to hold each item on "paper" from Material UI

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { BasketContext } from "../contexts/BasketContext";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Paper } from "@mui/material";
 
 function Basket() {
     const { basket, updateQuantity, calculateTotal, deleteItem } = useContext(BasketContext);
@@ -15,6 +16,7 @@ function Basket() {
                 <div>
                     {basket.map((item) => (
                         <div key={item.id} className="basket-item">
+                            <Paper elevation={12}>
                             <h3>{item.name}</h3>
                             <p>Price: £{item.price}</p>
                             <label>
@@ -28,6 +30,7 @@ function Basket() {
                             <IconButton aria-label="delete" onClick={() => deleteItem(item.id)}>
                                 <DeleteIcon />
                             </IconButton>
+                            </Paper>
                         </div>
                     ))}
                     <h2 className="basket-total">Total: £{calculateTotal()}</h2>
